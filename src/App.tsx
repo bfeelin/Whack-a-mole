6262f4d1da1c166201b9aa9657dc85bd61b498e2
Brian Phelan <brianphelan59@gmail.com>
2023-11-05 13:54:29 -0500

    Add interval to set a random tile to active every 1 second

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -22,14 +22,36 @@ function App() {
 
 function WhackAMole({initialData}: {initialData:Array<Tile>}){
   const [score, setScore] = useState<number>(0)
-  const [tiles, setTiles] = useState(initialData)
+  const [tiles, setTiles] = useState<Array<Tile>>(initialData)
+  const [delay, setDelay] = useState<number>(1000)
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
+      let randomTile = getRandomArbitrary(0, 8);
+      console.log(randomTile)
+      let newTiles = tiles?.map((tile) => ({
+          id: tile.id,
+          active: randomTile === tile.id ? true : false
+        }
+      ))
+      setTiles(newTiles)
+    }, delay)
+  
+    return () => clearInterval(intervalId); //This is important
+   
+  }, [])
+
+  function getRandomArbitrary(min:number, max:number) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  // Increment score and set the clicked tile back to inactive
   function handleIncrementScore(tileID:number){
     let newTiles = [...tiles]
     newTiles[tileID] = {...newTiles[tileID], active: false}
     setTiles(newTiles)
-    let s = score
-    setScore(s++)
+    let s = score + 1
+    setScore(s)
   }
   return (
     <>
